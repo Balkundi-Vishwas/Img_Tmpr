800f7c3fced7198d382005d9e929e84a22a73368
Balkundi-Vishwas <95179937+Balkundi-Vishwas@users.noreply.github.com>
2023-07-01 22:38:20 +0530

    Delete myApp directory
    efewf

--- myApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-
-urlpatterns = [
-    path('', views.homepage),
-    path('result/', views.result, name="result")
-]
